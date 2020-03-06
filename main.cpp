a62d2403c26266d32289e94ea272312c5fe1935a
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-06 23:46:38 +0300

    Задание2(e)

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ cout <<"*********"<<endl;
 cout <<"        *"<<endl;
 cout <<"*********"<<endl;
 
+double a,h;
+cout<<"Please,enter a = ";
+cin>>a;
+cout<<"Please,enter h = ";
+cin>>h;
+double p=a*4;double s=a*h;
+cout<<"Perimeter="<<p<<endl;
+cout<<"Square="<<s<<endl;
+
+
 return 0;
 
 
