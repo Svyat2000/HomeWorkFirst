0290ea0ba796e4c5c5cb24e5dba463236cadec0c
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-06 23:44:53 +0300

    Задание1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+cout <<"*********"<<endl;
+cout <<"*       "<<endl;
+cout <<"*********"<<endl;
+cout <<"        *"<<endl;
+cout <<"*********"<<endl;
+
+return 0;
+
+
+
+
+}
+
