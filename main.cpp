2a16d2687a99d7eb8a5b0f8a94112befeb5db8dc
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-06 23:53:39 +0300

    Задание8(e)

--- main.cpp
+++ main.cpp
@@ -10,14 +10,18 @@ cout <<"*********"<<endl;
 cout <<"        *"<<endl;
 cout <<"*********"<<endl;
 
-double a,h;
-cout<<"Please,enter a = ";
-cin>>a;
-cout<<"Please,enter h = ";
-cin>>h;
-double p=a*4;double s=a*h;
-cout<<"Perimeter="<<p<<endl;
-cout<<"Square="<<s<<endl;
+//double a,h;
+//cout<<"Please,enter a = ";
+//cin>>a;
+//cout<<"Please,enter h = ";
+//cin>>h;
+//double p=a*4;double s=a*h;
+//cout<<"Perimeter="<<p<<endl;
+//cout<<"Square="<<s<<endl;
+
+double x=3.6;
+double answ=exp(x-2)+fabs(sin(x))-pow(x,4)*cos(1/x);
+cout<<answ;
 
 
 return 0;
