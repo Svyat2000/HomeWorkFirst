4245fd064b7b44e7ec5e5b2c230bfcb7a16cf975
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-07 09:40:23 +0300

    Задание7

--- main.cpp
+++ main.cpp
@@ -23,21 +23,29 @@ cout <<"*********"<<endl;
 //double answ=exp(x-2)+fabs(sin(x))-pow(x,4)*cos(1/x);
 //cout<<answ;
 
-int a;
-double b;
-cout<<"Make a choise:"<<endl;cout<<"Tea spoon - 1"<<endl;cout<<"Tablespoon - 2"<<endl;cout<<"Mug - 3"<<endl;
-cin>>a;
-cout<<"Please enter quantity: ";
-cin>>b;
-if(a==1){
-    cout<<"in a tablespoon: "<<b*3<<endl;cout<<"in a mug: "<<b*48;
-}
-if(a==2){
-   cout<<"in a tea spoon: "<<b/3<<endl;cout<<"in a mug: "<<b*16;
-}
-if(a==3){
-    cout<<"in a tea spoon: "<<b/48<<endl;cout<<"in a tablespoon: "<<b/16;
-}
+//int a;
+//double b;
+//cout<<"Make a choise:"<<endl;cout<<"Tea spoon - 1"<<endl;cout<<"Tablespoon - 2"<<endl;cout<<"Mug - 3"<<endl;
+//cin>>a;
+//cout<<"Please enter quantity: ";
+//cin>>b;
+//if(a==1){
+    //cout<<"in a tablespoon: "<<b*3<<endl;cout<<"in a mug: "<<b*48;
+//}
+//if(a==2){
+  // cout<<"in a tea spoon: "<<b/3<<endl;cout<<"in a mug: "<<b*16;
+//}
+//if(a==3){
+    //cout<<"in a tea spoon: "<<b/48<<endl;cout<<"in a tablespoon: "<<b/16;
+//}
+
+double n;
+cout<<"Insert the number:  "<<endl;
+cin>>n;
+cout<<"Square of your number: "<<pow(n,2)<<endl;
+cout<<"Cube of your number: "<<pow(n,3);
+
+
 
 
 
